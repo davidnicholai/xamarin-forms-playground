eb6730fc5b752c9f7e48fb17a6802dfa09c5b3c5
David Lim <nicholai.david@gmail.com>
2019-01-12 17:24:53 +0800

    Initial commit. Study on ActivityIndicator.

---
+++ Playground/BindableBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Playground
+{
+    public class BindableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return;
+
+            field = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Playground/MainPageVIewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Playground
+{
+    public class MainPageViewModel : BindableBase
+    {
+        public ICommand IsRunningCommand { get; set; }
+        public ICommand IsVisibleCommand { get; set; }
+
+        private bool _isRunningSpinner;
+        public bool IsRunningSpinner
+        {
+            get { return _isRunningSpinner; }
+            set { SetProperty(ref _isRunningSpinner, value); }
+        }
+
+        private bool _isVisibleSpinner;
+        public bool IsVisibleSpinner
+        {
+            get { return _isVisibleSpinner; }
+            set { SetProperty(ref _isVisibleSpinner, value); }
+        }
+
+        private string _isRunningText;
+        public string IsRunningText
+        {
+            get { return _isRunningText; }
+            set { SetProperty(ref _isRunningText, value); }
+        }
+
+        private string _isVisibleText;
+        public string IsVisibleText
+        {
+            get { return _isVisibleText; }
+            set { SetProperty(ref _isVisibleText, value); }
+        }
+
+        public MainPageViewModel()
+        {
+            IsRunningCommand = new Command(() =>
+            {
+                IsRunningSpinner = !IsRunningSpinner;
+                IsRunningText = "IsRunning: " + IsRunningSpinner.ToString();
+            });
+
+            IsVisibleCommand = new Command(() =>
+            {
+                IsVisibleSpinner = !IsVisibleSpinner;
+                IsVisibleText = "IsVisible: " + IsVisibleSpinner.ToString();
+            });
+
+            IsRunningText = "IsRunning: " + IsRunningSpinner.ToString();
+            IsVisibleText = "IsVisible: " + IsVisibleSpinner.ToString();
+        }
+    }
+}
