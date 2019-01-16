57bc9c183a2ca5df6118fc961d4fcad2c679081a
David Lim <nicholai.david@gmail.com>
2019-01-16 23:54:22 +0800

    More study on Tasks

---
+++ Playground/Tasks/SillySingleton.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Playground.MiscUtilities;
+using Playground.Models;
+
+namespace Playground.Tasks
+{
+    public class SillySingleton
+    {
+        private static SillySingleton _instance = null;
+
+        public static SillySingleton Instance
+        {
+            get { return _instance ?? new SillySingleton(); }
+        }
+
+        private Task<Weather> _loadWeatherTask = null;
+        private RestService _restService = null;
+
+        public SillySingleton()
+        {
+            _restService = new RestService();
+        }
+
+        public Task<Weather> LoadWeather()
+        {
+            if (_loadWeatherTask == null)
+            {
+                _loadWeatherTask = Task.Run(async () =>
+                {
+                    string apiKey = "";
+                    string urlTemplate = @"https://api.openweathermap.org/data/2.5/weather?q=Manila&appid={0}";
+                    string url = string.Format(urlTemplate, apiKey);
+
+                    System.Diagnostics.Debug.WriteLine("Crap i am running again");
+
+                    var weather = await _restService.GetRequest<Weather>(url);
+                    return weather;
+                });
+            }
+
+            return _loadWeatherTask;
+        }
+    }
+}
--- Playground/Tasks/TasksPageViewModel.cs
+++ Playground/Tasks/TasksPageViewModel.cs
@@ -8,89 +8,24 @@ namespace Playground.Tasks
 {
     public class TasksPageViewModel : BindableBase
     {
-        private Task _myTask;
-
         public ICommand StartTaskCommand { get; set; }
-        public ICommand StartTaskAsyncCommand { get; set; }
-        public ICommand RefreshStatusCommand { get; set; }
-
-        private RestService _restService;
-
-        private string _taskStatus;
-        public string TaskStatus
-        {
-            get { return _taskStatus; }
-            set { SetProperty(ref _taskStatus, value); }
-        }
-
-        private string _weatherData;
-        public string WeatherData
-        {
-            get { return _weatherData; }
-            set { SetProperty(ref _weatherData, value); }
-        }
+        public ICommand StartTaskCommandAwaited { get; set; }
 
         public TasksPageViewModel()
         {
-            StartTaskCommand = new Command(() => StartTask());
-            StartTaskAsyncCommand = new Command(async () => await StartTaskAsync());
-            RefreshStatusCommand = new Command(() => RefreshStatus());
-            _restService = new RestService();
-            RefreshStatus();
-        }
-
-        private Task StartTask()
-        {
-            bool hasCompleted = false;;
-            if (_myTask != null && _myTask.IsCompleted)
+            StartTaskCommand = new Command(() =>
             {
-                hasCompleted = true;
-            }
+                SillySingleton.Instance.LoadWeather();
+                SillySingleton.Instance.LoadWeather();
+                SillySingleton.Instance.LoadWeather();
+            });
 
-            _myTask = FetchWeatherData(hasCompleted);
-
-            return _myTask;
-        }
-
-        private async Task StartTaskAsync()
-        {
-            Weather weather = await FetchWeatherDataAsync();
-
-            WeatherData = weather != null
-                ? $"Fetched! Name: {weather.Name}"
-                : "weather is null";
-        }
-
-        private Task<Weather> FetchWeatherData(bool hasCompleted)
-        {
-            if (hasCompleted)
+            StartTaskCommandAwaited = new Command(async () =>
             {
-                System.Diagnostics.Debug.WriteLine("The task is already IsCompleted but ok");
-            }
-            string apiKey = "";
-            string urlTemplate = @"https://api.openweathermap.org/data/2.5/weather?q=Manila&appid={0}";
-            string url = string.Format(urlTemplate, apiKey);
-
-            System.Diagnostics.Debug.WriteLine("Crap i am running again");
-
-            return _restService.GetRequest<Weather>(url);
-        }
-
-        private async Task<Weather> FetchWeatherDataAsync()
-        {
-            string apiKey = "";
-            string urlTemplate = @"https://api.openweathermap.org/data/2.5/weather?q=Manila&appid={0}";
-            string url = string.Format(urlTemplate, apiKey);
-
-            return await _restService.GetRequest<Weather>(url);
-        }
-
-
-        private void RefreshStatus()
-        {
-            TaskStatus = _myTask != null
-                ? $"IsCompleted: {_myTask.IsCompleted} | IsCanceled: {_myTask.IsCanceled} | IsFaulted: {_myTask.IsFaulted}" 
-                : "_myTask is null";
+                await SillySingleton.Instance.LoadWeather();
+                await SillySingleton.Instance.LoadWeather();
+                await SillySingleton.Instance.LoadWeather();
+            });
         }
     }
 }
